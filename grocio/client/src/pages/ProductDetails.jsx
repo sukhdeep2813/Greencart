a3e54365702d37533b530f0232c657c352082b6e
sukhdeep <sukhdeepkumar918@gmail.com>
2026-03-24 21:54:05 +0530

    Index Allocation to Star

--- grocio/client/src/pages/ProductDetails.jsx
+++ grocio/client/src/pages/ProductDetails.jsx
@@ -65,6 +65,7 @@ const ProductDetails = () => {
                 .fill("")
                 .map((_, i) => (
                   <img
+                    key={i}
                     src={i < 4 ? assets.star_icon : assets.star_dull_icon}
                     alt=""
                     className="md:w-4 w-3.5"
